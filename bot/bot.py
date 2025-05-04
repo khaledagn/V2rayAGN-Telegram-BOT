28b0e47eec5bad5399cccfc85e8852c1e1cdf061
Khaled AGN <68344344+khaledagn@users.noreply.github.com>
2025-05-04 15:43:54 +0100

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,7 +2,7 @@ from bot import get_bot_updater
 from bot.handlers import start, send_configs
 from telegram.ext import CommandHandler, CallbackQueryHandler
 
-BOT_TOKEN = "your_bot_token"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7798305127:AAGmK30Uwk8O1ES9lvGOXghWoYm03Cd0lrI") 
 
 def main():
     updater = get_bot_updater(BOT_TOKEN)
